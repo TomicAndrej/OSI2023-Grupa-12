3451cb0e1edd04ebf52a251b7de34f16af5e1616
TomicAndrej <148867716+TomicAndrej@users.noreply.github.com>
2024-01-08 12:45:56 +0100

    Napravljena dva odvojena projekta

---
+++ Sistem_za_registraciju_vozila/sistem_za_registraciju_vozila/Form1.cs
@@ -0,0 +1,10 @@
+namespace sistem_za_registraciju_vozila
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Sistem_za_tehnicki_pregled/sistem_za_tehnicki_pregled/Form1.cs
@@ -0,0 +1,10 @@
+namespace sistem_za_tehnicki_pregled
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
